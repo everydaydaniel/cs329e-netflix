4625e9deb5d54a845f94db637e2b684420be44e4
everydaydaniel <everydaydaniel1994@gmail.com>
2017-02-13 19:00:30 -0600

    corrected Netflix.py

--- Netflix.py
+++ Netflix.py
@@ -58,28 +58,40 @@ def createPersonal_cache():
         count += 1
         cache_key = key
         val = avgRateYear_dict[cache_key[1]]
-        print(key, val)
+        #print(key, val)
 
         try:
             custAverage = custAvgRateYear_dict[key]
             yearAverage = avgRateYear_dict[key[1]]
-            cache[key] = custAverage - yearAverage#customer yearly - yearly average
+            cache[key] = (custAverage + yearAverage) / 2
+            #old code == custAverage - yearAverage
+            #customer yearly - yearly average
 
         except Exception as e:
             print("Failed", e)
-    pickle_out = open("CustomerOffsetByYear.pickle","wb")
+    pickle_out = open("CombinedAveragePerYear.pickle","wb")
     pickle.dump(cache, pickle_out)
     pickle_out.close()
 
-def getPersonal_cache():
+
+def getPersonal_cache(num):
     """
     USE THIS TO CREATE THE CACHE CUSTOMMER OFFSET CACHE
     Deserializes Customer Offset cache
+    cache Schema: {(customerID,Year): OFFSET }
+    returns as  : {     (Tuple)     : float  }
     """
-    infile = open("CustomerOffsetByYear.pickle","rb")
+    avaliable_files = ["CombinedAveragePerYear.pickle","CustomerOffsetByYear.pickle", "kzk66-movies_and_years.pickle"]
+    
+    num = int(num)
+    inputFile = avaliable_files[num]
+    infile = open(inputFile ,"rb")
     cache = pickle.load(infile)
     infile.close()
     return cache
+cache = getPersonal_cache()
+print(len(cache))
+
 
 
 
